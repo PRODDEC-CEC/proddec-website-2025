9be9697f9d7dd857a490add03cc51395e4050d4c
Viswaprabha <viswaprabha1879@gmail.com>
2024-12-27 17:27:26 +0530

    Set up Routing and basic features.

---
+++ src/app/about/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Metadata } from "next"
+
+export const metadata = {
+  title: 'About Us - PRODDEC CEC'
+}
+
+
+const About = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
