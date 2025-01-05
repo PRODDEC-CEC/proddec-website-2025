fe66f1e79c5c235ef19291c6f4baea169a571e8f
Abhijith_J_Nair <136841851+ACE-Z666@users.noreply.github.com>
2025-01-05 16:34:08 +0530

    images added

--- src/app/_components/events.jsx
+++ src/app/_components/events.jsx
@@ -4,7 +4,7 @@ import "../globals.css"
 const add1 = () => {
   return (
     <div className="bg-[#000000] h-[100vh] w-[100vw]">
-
+      <h1 className="text-3xl font-bold text-white ">Events</h1>
     </div>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import react from "react";
 import Hero from "./_components/Hero/hero"
-import Add1 from "./_components/add1"
+import Add1 from "./_components/events"
 import Navbar from "./_components/navbar";
 import "./globals.css"
 
