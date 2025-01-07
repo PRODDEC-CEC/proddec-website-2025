d82ac7a370684d046c51662adf71029aebb56036
Abhijith_J_Nair <136841851+ACE-Z666@users.noreply.github.com>
2025-01-07 23:58:07 +0530

    Animated the events section

--- src/app/_components/Events/events.jsx
+++ src/app/_components/Events/events.jsx
@@ -1,7 +1,89 @@
+"use client";
+
 import React from 'react'
 import "./events.css"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { useGSAP } from '@gsap/react'
 
 const add1 = () => {
+
+  useGSAP(() => {
+
+    gsap.registerPlugin(ScrollTrigger);
+  
+    gsap.from(".evntimg1",{
+      y: -100,
+      duration: 1,
+      delay: 0.5,
+      scrollTrigger :({
+        trigger : "#evntcrd",
+        start: "top 100%",
+        end: "bottom 20%",
+        scrub: 3,
+        // markers: true
+      })
+    }),
+
+    gsap.from(".evntimg2",{
+      x: -100,
+      duration: 1,
+      delay: 0.5,
+      ease: "power2.inOut",
+      scrollTrigger :({
+        trigger : "#evntcrd",
+        start: "top 100%",
+        end: "bottom 20%",
+        scrub: 3,
+        // markers: true
+      })
+    }),
+
+    gsap.from(".evntimg3",{
+      y: 300,
+      duration: 1,
+      delay: 0.5,
+      scrollTrigger :({
+        trigger : "#evntcrd",
+        start: "top 100%",
+        end: "bottom 20%",
+        scrub: 3,
+        // markers: true
+      })
+    }),
+
+    gsap.from(".evntimg4",{
+      x: 500,
+      duration: 1,
+      delay: 0.5,
+      scrollTrigger :({
+        trigger : "#evntcrd",
+        start: "top 100%",
+        end: "bottom 20%",
+        scrub: 3,
+        // markers: true
+      })
+    }),
+
+    gsap.from(".evntimg5",{
+      x: -900,
+      duration: 1,
+      delay: 0.5,
+      scrollTrigger :({
+        trigger : "#evntcrd",
+        start: "top 100%",
+        end: "bottom 20%",
+        scrub: 3,
+        // markers: true
+      })
+    })
+  
+  
+  
+  
+  
+  })
+  
   return (
     <div className="bg-[#111111] h-[120vh] pt-36 w-[100vw] overflow-hidden">
       
