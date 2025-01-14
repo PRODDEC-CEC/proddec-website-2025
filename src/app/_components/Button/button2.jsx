959f53e0c23b2b8ac0dd4aafb9ffe164e85c3c3f
Abhijith_J_Nair <136841851+ACE-Z666@users.noreply.github.com>
2025-01-14 20:56:07 +0530

    Hero Section

---
+++ src/app/_components/Button/button2.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import './button1.css'
+
+const button = () => {
+  return (
+<button>Know More</button>
+  )
+}
+
+export default button
--- src/app/_components/Hero/hero.jsx
+++ src/app/_components/Hero/hero.jsx
@@ -7,6 +7,7 @@ import HeroAbstract from "./abstract.jsx"
 import { useGSAP } from '@gsap/react'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import Btn1 from "../Button/button1.jsx"
+import Btn2 from "../Button/button2.jsx"
 
 
 const hero = () => {
@@ -160,9 +161,13 @@ const hero = () => {
     {/* The About Section */}
     <div className='hero2'>
       <div id='imglft'>
-          <h1>ABOUT <span> PRODDEC</span></h1>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+          <h1>ABOUT <span>PRODDEC</span></h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+          <div className='btn2'>
+        <Btn2></Btn2>
       </div>
+      </div>
+     
       <div id='imgrig'>
         <div className="imgcntr"></div>
         <div className="imgcntr"></div>
