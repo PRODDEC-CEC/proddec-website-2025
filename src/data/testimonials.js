a9b1ad75d77859c0969ce741119c390f558a3f7f
Alwin Saji <alwinsaji4.cgnr@gmail.com>
2026-03-04 23:41:47 +0530

    v4.7

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -2,14 +2,14 @@ export const testimonials = [
   {
     id: 1,
     name: "Abhijith J Nair",
-    role: "Former President",
+    role: "President 25-26",
     quote: "This Forum has a Legacy of 30 years, and I believe the new Dawn of PRODDEC will foster its Legacy for Future.",
     image: "https://api.dicebear.com/7.x/avataaars/svg?seed=Alex"
   },
   {
     id: 2,
     name: "Jithu Girish",
-    role: "Former R & D Software Lead",
+    role: "R & D Software Lead 25-26",
     quote: "Being a part of Proddec has been a great experience in my life. The forum helped me work on real-world projects and participate in future-ready workshops. Proddec is truly one of the best platforms to learn, innovate, and gain skills that prepare you for the future as an engineer.",
     image: "https://api.dicebear.com/7.x/avataaars/svg?seed=Sarah"
   },
